28646b92d3db03a5b1f2ca0c0712799199fc6ede
Dino <59518803+d3noDL@users.noreply.github.com>
2023-01-17 12:52:50 +0100

    Added probuilder and super controller

---
+++ Assets/Scripts/MAIN.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MAIN : MonoBehaviour
+{
+    public static MAIN i = new MAIN();
+    
+    
+    
+}
